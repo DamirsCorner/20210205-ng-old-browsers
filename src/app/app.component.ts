18971b9a4462544f78a698889da53232a9404e10
Damir Arh <damir.arh@gmail.com>
2021-01-16 09:53:26 +0100

    Modern ES without polyfill
    Breaks in IE11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-old-browsers';
+
+  ngOnInit(): void {
+    const params = {
+      key1: 'value1',
+      key2: 'value2',
+      key3: 'value3',
+    };
+
+    const queryString = Object.entries(params)
+      .map(([key, value]) => `${key}=${value}`)
+      .join('&');
+  }
 }
