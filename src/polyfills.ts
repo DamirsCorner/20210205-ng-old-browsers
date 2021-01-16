c7b56dab1c04140c11dc6884a9697938926a2567
Damir Arh <damir.arh@gmail.com>
2021-01-16 09:58:17 +0100

    Polyfills make modern JS work in IE

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -52,11 +52,12 @@
  *
  */
 
+import 'core-js/features/object/entries';
+
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
+import 'zone.js/dist/zone'; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
